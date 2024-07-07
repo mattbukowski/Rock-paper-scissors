1c3244a15aac2206d724f5aa1f1675966c2457df
mattbukowski <mattbukowski98@gmail.com>
2024-07-07 19:22:30 +0200

    Add user choice and return it
    I created var to store the user input and function to call it

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
-// Creat variable to store computer choice
-// Then creat function to call for computer choice
+// Create variable to store computer choice
+// Then create function to call for computer choice
 // Now I want to assign numerical computer choice to one of three values: "rock", "paper" or "scissors", I will use 'if' statement
 
 let computerChoice = 0;
@@ -15,4 +15,20 @@ function getComputerChoice () {
     }
 }
 
+// Create variable to store human choice from user input
+// Create function to call for human choice
+
+let humanChoice = prompt("Choose: Rock/Paper/Scissors").toLowerCase();
+
+function getHumanChoice () {
+    if (humanChoice == "rock") {
+        return "Rock";
+    } else if (humanChoice == "paper") {
+        return "Paper";
+    } else if (humanChoice == "scissors") {
+        return "Scissors";
+    }
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
