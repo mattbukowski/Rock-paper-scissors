92bd3370c94c5537e5d422e09549610a1f9885bf
mattbukowski <mattbukowski98@gmail.com>
2024-07-07 17:43:19 +0200

    Add func for getting rock/paper/scissors from comp
    I wrote var for containing choice of computer generated in func to randomly choose between num 0-2 and return values as "rock", "paper" or "scissors"

--- javascript.js
+++ javascript.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+// Creat variable to store computer choice
+// Then creat function to call for computer choice
+// Now I want to assign numerical computer choice to one of three values: "rock", "paper" or "scissors", I will use 'if' statement
+
+let computerChoice = 0;
+
+function getComputerChoice () {
+    computerChoice = Math.floor(Math.random() * (4-1))
+    if (computerChoice == 0) {
+        return "Rock";
+    } else if (computerChoice == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
