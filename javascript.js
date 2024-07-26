10972e373bd7116da949bf3bf4107d8848cc4daf
mattbukowski <mattbukowski98@gmail.com>
2024-07-26 12:24:16 +0200

    Add clickable ui
    Changes code drasticly cause I had to seek for help in the Net. Clickable icons added, as well as UI

--- javascript.js
+++ javascript.js
@@ -1,80 +1,65 @@
-//I will start over here
-// Create new variables for human and computer scores, also for round result
+//Reference to the buttons, getting me acces to it
+const buttons = document.querySelectorAll("[data-selection]")
 
-let playRoundResult;
-let computerScore = 0;
-let humanScore = 0;
-
-//Create variable to store computer choice
-//Create funtion to get computer choice
-
-//let choice = Math.floor(Math.random() * 3) + 1;
-
-//function getComputerChoice() {
-//if (choice === 1) return "ROCK";
-// if (choice === 2) return "PAPER";
-// return "SCISSORS";
-//}
-
-//Create variable to store human choice
-
-//let humanChoice = prompt('Choose: Rock/Paper/Scissors', '');
-
-//Assign human and computer choices to the variables
-//Human choice is turn to upper case
-//const playerSelection = humanChoice.toUpperCase();
-//const computerSelection = getComputerChoice();
-
-//Create function to play single round
-
-//function playRound(playerSelection, computerSelection) {
-//const win = (computerSelection === "ROCK" && playerSelection === "PAPER") || (computerSelection === 'SCISSOR' && playerSelection === //'ROCK') || (computerSelection === 'PAPER' && playerSelection === 'SCISSOR');
-//if (computerSelection === playerSelection) return "It's a draw :|";
-//if (win) {
-//    humanScore++;
-//    return "You win!";
-//}
-//computerScore++;
-//return "You lose!";
-//}
-
-//Assign func playRound to the variable playRoundResult
-
-//playRoundResult = playRound(playerSelection, computerSelection);
-
-
-//Create function for 5 round game
-
-const playGame = function playGame() {
-    for (let i = 0; i < 5; i++) {
-        let choice = Math.floor(Math.random() * 3) + 1;
-        function getComputerChoice() {
-            if (choice === 1) return "ROCK";
-            if (choice === 2) return "PAPER";
-            return "SCISSORS";
-        }
-        function playRound(playerSelection, computerSelection) {
-            const win = (computerSelection === "ROCK" && playerSelection === "PAPER") || (computerSelection === 'SCISSORS' && playerSelection === 'ROCK') || (computerSelection === 'PAPER' && playerSelection === 'SCISSORS');
-            if (computerSelection === playerSelection) return "It's a draw :|";
-            if (win) {
-                humanScore++;
-                return "You win!";
-            }
-            computerScore++;
-            return "You lose!";
-        }
-        let humanChoice = prompt('Choose: Rock/Paper/Scissors', '');
-        const playerSelection = humanChoice.toUpperCase();
-        const computerSelection = getComputerChoice();
-        playRoundResult = playRound(playerSelection, computerSelection);
-
-        console.log(playRound(playerSelection, computerSelection));
-        console.log(`Player pick: ${playerSelection}`);
-        console.log(`Computer pick: ${computerSelection}`);
+const finalColumn = document.querySelector("[data-final-column]")
+const computerScore = document.querySelector("[data-computer-score]")
+const yourScore = document.querySelector("[data-your-score]")
+//Variable of possible choices with information who beats who
+const CHOICES = [
+    {
+        name: "ROCK",
+        emoji: "✊",
+        beats: "SCISSORS",
+    },
+    {
+        name: "PAPER",
+        emoji: "✋",
+        beats: "ROCK",
+    },
+    {
+        name: "SCISSORS",
+        emoji: "✌️",
+        beats: "PAPER",
     }
+]
+//Give me information what was clicked by user
+buttons.forEach(button => {
+    button.addEventListener("click", e => {
+        const selectionName = button.dataset.selection
+        const selection = CHOICES.find(selection => selection.name === selectionName)
+        playRound(selection);
+    })
+})
+
+function playRound(selection) {
+    const computerChoice = getComputerChoice()
+    const userWin = isWinner(selection, computerChoice);
+    const computerWin = isWinner(computerChoice, selection);
+
+    addSelectionResult(computerChoice, computerWin)
+    addSelectionResult(selection, userWin)
+
+    if (userWin) incrementScore(yourScore)
+    if (computerWin) incrementScore(computerScore)
 }
 
-playGame();
+function incrementScore(score) {
+    score.innerText = parseInt(score.innerText) + 1
+}
 
-console.log(`Computer score: ${computerScore}`);
-console.log(`Player score: ${humanScore}`);
+function addSelectionResult(selection, winner) {
+    const div = document.createElement("div")
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if (winner) div.classList.add('winner')
+    finalColumn.after(div)
+}
+
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * CHOICES.length);
+    return CHOICES[choice];
+}
+
+function isWinner(selection, computerChoice) {
+    return selection.name === computerChoice.beats
+}
